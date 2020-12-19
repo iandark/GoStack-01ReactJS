0705a0656418ab9d4f4603e8157e80e6e2582618
Derkian Souza <derkian@iands.com.br>
2020-12-19 20:42:53 -0300

    feat: Inclusão de webpack-merge

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,45 +1,10 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.config.common');
 
-module.exports = {
+module.exports = merge(commonConfig, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.resolve(__dirname, "dist"),
-  },
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      inject: true,
-      template: path.resolve(__dirname, 'src', 'index.html'),
-    }),
-  ],
-  node: {
-    fs: "empty"
-  },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-            },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: [{ loader: "style-loader" }, { loader: "css-loader" }],
-            },
-            {
-                test: /.*\.(gif|png|jpe?g)$/i,
-                use: { loader: "file-loader" },
-            },
-        ],
-    }
-}
+    contentBase: "./dist",
+  }
+});
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,42 +1,7 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge')
+const commonConfig = require('./webpack.config.common');
 
-module.exports = {
+module.exports = merge(commonConfig, {
     mode: 'production',
-    devtool: 'source-map',
-    entry: path.resolve(__dirname, "src", "index.js"),
-    node: {
-        fs: "empty"
-    },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "main.js",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: { loader: "babel-loader" },
-            },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: [{ loader: "style-loader" }, { loader: "css-loader" }],
-            },
-            {
-                test: /.*\.(gif|png|jpe?g)$/i,
-                use: { loader: "file-loader" },
-            },
-        ],
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            inject: true,
-            template: path.resolve(__dirname, 'src', 'index.html'),
-        }),
-    ]
-};
+    devtool: 'source-map'
+});
